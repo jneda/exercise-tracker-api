d5a9a765a798fe75c920f3644180bc16e321b0ba
Julien Dané <julien.dane@gmail.com>
2023-03-03 16:00:42 +0100

    set up User model

---
+++ migrations/20230303144209-create-user.js
@@ -0,0 +1,28 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      _id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
---
+++ seeders/20230303144933-User.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const { v4: uuidv4 } = require("uuid");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          username: "Athony Houlala",
+          _id: uuidv4(),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          username: "Pascaline Démonte-Pneu",
+          _id: uuidv4(),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
