6b63aac4f603fbf68fc7b9b87a865f7663a9fcfb
Julien Dané <julien.dane@gmail.com>
2023-03-03 17:26:13 +0100

    use morgan for logging use jest and supertest for testing

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const logger = require("morgan");
 
 const apiRoutes = require("./routes");
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,7 +19,7 @@ app.get("/", (req, res) => {
 
 // catch all route
 app.use((req, res, next) => {
-  res.status(404).send("Resource could not be found.")
-})
+  res.status(404).send("Resource could not be found.");
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "Hello from the API." }));
+router.get("/", (req, res) => res.status(200).json({ message: "Hello from the API." }));
 
 module.exports = router;
