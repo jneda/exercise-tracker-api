a87cf2605271f0c7a485025f3cb0a081517602e9
Julien Dané <julien.dane@gmail.com>
2023-03-03 17:02:08 +0100

    setup API routes

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const apiRoutes = require("./routes");
+
 const app = express();
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api", apiRoutes);
 
 app.get("/", (req, res) => {
   res.send("This is root.");
 });
+
+// catch all route
+app.use((req, res, next) => {
+  res.status(404).send("Resource could not be found.")
+})
 
 module.exports = app;
